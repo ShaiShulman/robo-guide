0676c452352ba46933f2f058489b50be54a82af2
shai kipod <ShaiShulman@gmail.com>
2023-12-22 16:29:11 +0200

    Remove robo-tourist subfolder

--- robo-guide/frontend/vite.config.ts
+++ robo-guide/frontend/vite.config.ts
@@ -11,13 +11,11 @@ export default defineConfig({
     host: true, // needed for the Docker Container port mapping to work
     strictPort: true,
     port: 2000, // you can replace this port with any port
-    proxy: {
-      // Proxy options here
-      "/api": {
-        target: "https://roboguide.xyz/api",
-        // target: "http://localhost:2500",
-        changeOrigin: true,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:8000",
+    //     changeOrigin: true,
+    //   },
+    // },
   },
 });
